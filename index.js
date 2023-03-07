bd38f05c4e48dba7e39296c7ac3c9a0fd30b92c0
Carlos Bahena <carlosbahmar@gmail.com>
2023-03-06 20:34:09 -0600

    se añaden const de los botones

---
+++ index.js
@@ -0,0 +1,31 @@
+let displayValue = 0;
+let valorA = 0;
+let valorB = 0;
+let temp = 0;
+const button1 = document.querySelector('.button-1');
+const button2 = document.querySelector('.button-2');
+const button3 = document.querySelector('.button-3');
+const button4 = document.querySelector('.button-4');
+const button5 = document.querySelector('.button-5');
+const button6 = document.querySelector('.button-6');
+const button7 = document.querySelector('.button-7');
+const button8 = document.querySelector('.button-8');
+const button9 = document.querySelector('.button-9');
+const button0 = document.querySelector('.button-0');
+const button00 = document.querySelector('.button-00');
+const button000 = document.querySelector('.button-000');
+
+const buttonKentreAtk = document.querySelector('.button-kentre-atk');
+const buttonPunisherAtk = document.querySelector('.button-punisher-atk');
+const buttonKentreDmg = document.querySelector('.button-kentre-dmg');
+
+const buttonBack = document.querySelector('.button-back');
+const buttonClear = document.querySelector('.button-clear');
+
+const buttonDinoRest = document.querySelector('.button-dino-rest');
+const buttonDinoAdd = document.querySelector('.button-dino-add');
+const buttonDinoHalf = document.querySelector('.button-dino-half');
+
+const buttonOpRest = document.querySelector('.button-op-rest');
+const buttonOpAdd = document.querySelector('.button-op-add');
+const buttonOpHalf = document.querySelector('.button-op-half');
